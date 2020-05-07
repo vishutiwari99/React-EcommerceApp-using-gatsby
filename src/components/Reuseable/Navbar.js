de20b4ec656a1631f7328dbd3b40354115c7a930
techtiwari <vishutiwari99@gmail.com>
2020-05-08 00:21:57 +0530

    service has to be change

--- src/components/Reuseable/Navbar.js
+++ src/components/Reuseable/Navbar.js
@@ -21,7 +21,7 @@ export default class Navbar extends Component {
       {
         id: 3,
         text: "Services",
-        url: "/services",
+        url: "/service",
       },
       {
         id: 4,
--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -6,30 +6,28 @@ import SEO from "../components/seo"
 import HeroSection from "../components/Reuseable/HeroSection"
 import Infoblock from "../components/Reuseable/Infoblock"
 import Dualinfoblock from "../components/Reuseable/Dualinfoblock"
-import TeamPhotoSection from "../components/about/TeamPhotoSection"
 
-const AboutPage = ({ data }) => (
+const ServicePage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <HeroSection
       img={data.img.childImageSharp.fluid}
-      title="About LearnCodeOnline"
-      subtitle=""
-      heroclass="about-background"
+      title="I write Code"
+      subtitle="LearnCodeOnline.in"
+      heroclass="hero-background"
     />
+    <Infoblock heading="About us" />
     <Dualinfoblock
-      heading="A message from CEO"
+      heading="Our Services"
       cardname="Picture Chnaged"
       imgUrl="https://images.pexels.com/photos/1672637/pexels-photo-1672637.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     />
-    <Infoblock heading="About Vision" />
-    <TeamPhotoSection />
   </Layout>
 )
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "about.png" }) {
+    img: file(relativePath: { eq: "heromain.png" }) {
       childImageSharp {
         fluid(maxWidth: 400, maxHeight: 250) {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -39,4 +37,4 @@ export const query = graphql`
   }
 `
 
-export default AboutPage
+export default ServicePage
