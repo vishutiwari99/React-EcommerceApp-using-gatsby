1e308f851dedba816d7115de1e9696a3380fde06
techtiwari <vishutiwari99@gmail.com>
2020-05-08 16:41:26 +0530

    testing

---
+++ src/pages/ServicePage.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/Reuseable/HeroSection"
+import Infoblock from "../components/Reuseable/Infoblock"
+import Dualinfoblock from "../components/Reuseable/Dualinfoblock"
+
+const ServicePage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="I write Code"
+      subtitle="LearnCodeOnline.in"
+      heroclass="hero-background"
+    />
+    <Infoblock heading="Services" />
+    <Dualinfoblock
+      heading="Our Services"
+      cardname=""
+      imgUrl="https://images.pexels.com/photos/1672637/pexels-photo-1672637.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, maxHeight: 250) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default ServicePage
