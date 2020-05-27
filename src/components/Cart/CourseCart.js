1d089f5eb0bb2b17555286247699465fd82840e2
techtiwari <vishutiwari99@gmail.com>
2020-05-27 16:48:31 +0530

    inital commi

--- src/components/Cart/CourseCart.js
+++ src/components/Cart/CourseCart.js
@@ -19,19 +19,26 @@ class CourseCart extends Component {
           <div className="row">
             {this.state.mycourses.map(({ node }) => {
               return (
-                <div key={node.id} className="col-11 col-md-6 d-flex mx-auto">
+                <div key={node.id} className="col-11 col-md-6 d-flex my-3 mx-auto">
                   <Img fixed={node.image.fixed} />
                   <div className="flex-grow-1 px-3">
                     <div className="d-flex justify-content-between">
                       <h6 className="mb-0">{node.title}</h6>
                       <h6 className="mb-0 text-success"> $ {node.price}</h6>
-                      {/* <h6 className="mb-0">{node.title}</h6>
-                      <h6 className="mb-0">{node.title}</h6> */}
                     </div>
                     <p className="text-muted">
-                      <small>{node.desciption}</small>
+                      <small>{node.description.description}</small>
                     </p>
-                    <button className="btn btn-warning">join now</button>
+                    <button
+                      data-item-id={node.id}
+                      data-item-price={node.price}
+                      data-item-url="https://learncodeonline.in"
+                      data-item-image={node.image.fixed}
+                      data-item-name={node.title}
+                      className="btn btn-warning snipcart-add-item"
+                    >
+                      join now
+                    </button>
                   </div>
                 </div>
               )
